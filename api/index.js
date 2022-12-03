033bca76cb1090e71510584f2c3a1f54d9b21273
Dương Doãn <tranduongdoan0922@gmail.com>
2022-12-03 15:53:30 +0700

    bui 8

--- api/index.js
+++ api/index.js
@@ -1,17 +1,24 @@
-var promise = fetch("https://6044315ca20ace001728eb74.mockapi.io/api/world_cup");
-
+var promise = fetch("https://6044315ca20ace001728eb74.mockapi.io/api/phones");
+function renderRate(rate){
+  var htmlString = ""
+  for (var index = 0; index < rate; index++) {
+    htmlString += `<i class="fa-solid fa-star text-warning"></i>`
+  }
+  return htmlString
+}
 promise.then(function(response){
   return response.json()
 }).then(function(data){
-  console.log(data)
   var htmlString = '';
   for (let index = 0; index < data.length; index++) {
+    var rateHTMLString = renderRate(data[index].rate)
     htmlString += `
     <div class="card" style="width: 18rem;">
     <img src=${data[index].img} class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${data[index].name}</h5>
-      <h5 class="text-danger">${data[index].club}</h5>
+      <h5 class="text-danger">${data[index].description}</h5>
+      ${rateHTMLString}      
     </div>
     </div> 
     `
